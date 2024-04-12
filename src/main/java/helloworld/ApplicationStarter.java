a2d6bd8f62514339cd7269ab1bb32048814cd6c4
june-777 <wlwhswnsrl96@gmail.com>
2024-04-12 23:51:00 +0900

    feat: set-up spring-boot, jpa, mysql driver

---
+++ src/main/java/helloworld/ApplicationStarter.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
+public class ApplicationStarter {
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStarter.class, args);
+	}
+
+}
